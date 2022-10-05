4d32d47df24f9ed21138ad325c246c739fa48ad5
giacomo-dianese <109462523+giacomo-dianese@users.noreply.github.com>
2022-10-05 09:41:14 +0200

    cambio della persona

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const anni = 36;
-  const nome = 'Luca';
+  const anni = 42;
+  const nome = 'Marco';
   console.log('ciao il mio nome è '+nome+'e ho '+anni+'anni')
 });
